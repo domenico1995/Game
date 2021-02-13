12eaf63dbc4c4517c1bf3cb735295d8af5b6f93f
domenico1995 <domenicoromanazzi9@hotmail.com>
2021-02-13 18:42:01 +0100

    aggiunta di codice 13/02/21

--- client/src/main/java/com/client/main.java
+++ client/src/main/java/com/client/main.java
@@ -5,16 +5,34 @@
  */
 package com.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author domen
  */
 public class main {
-    
-    public static void main(String []args){
-    
+
+    public static void main(String[] args) throws IOException {
+
         System.out.println("hello world");
-    
+
+        ProcessBuilder builder = new ProcessBuilder(
+                "cmd.exe", "/c", "cd \"C:\\Users\\domen\\Desktop\\test\" && dir");
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) {
+                break;
+            }
+            System.out.println(line);
+        }
+
     }
-    
+
 }
